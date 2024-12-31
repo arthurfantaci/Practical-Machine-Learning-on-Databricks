26bdd375ced207cf22ab00f1d504d233b28df17b
arthurfantaci <arthur.fantaci@mac.com>
2024-12-31 10:23:23 -0500

    Update mlflow-with-featurestore.py

--- Chapter-04/mlflow-with-featurestore.py
+++ Chapter-04/mlflow-with-featurestore.py
@@ -50,7 +50,7 @@ MODEL_NAME = "random_forest_classifier_featurestore"
 #This is the name for the entry in model registry
 MODEL_REGISTRY_NAME = "Bank_Customer_Churn"
 #The email you use to authenticate in the Databricks workspace
-USER_EMAIL = "debu.sinha@databricks.com"
+USER_EMAIL = "arthur.fantaci@mac.com"
 #Location where the MLflow experiement will be listed in user workspace
 EXPERIMENT_NAME = f"/Users/{USER_EMAIL}/Bank_Customer_Churn_Analysis"
 # we have all the features backed into a Delta table so we will read directly
